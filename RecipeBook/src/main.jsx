ae33b6d47ec9dba9718fd0850f0ce2c02949ca16
lini bruno <linibruno1@gmail.com>
2024-07-10 23:24:25 +0200

    Initial commit - Set up React + Tailwind CSS

---
+++ RecipeBook/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='text-red-500'>Test</div>
+  </React.StrictMode>,
+)
